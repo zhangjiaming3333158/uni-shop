ed19c3408ef7e6c5dbbbfb7bf0c26b5ebd5245f4
zhangjiaming3333158 <1093561079@qq.com>
2023-08-02 15:37:23 +0800

    goods_list

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,89 @@
+<template>
+  <view>
+    <view class="goods-item" @click="gotoDetail">
+      <!-- 商品左侧图片区域 -->
+      <view class="goods-item-left">
+        <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+      </view>
+      <!-- 商品右侧信息区域 -->
+      <view class="goods-item-right">
+        <!-- 商品标题 -->
+        <view class="goods-name">{{goods.goods_name}}</view>
+        <view class="goods-info-box">
+          <!-- 商品价格 -->
+          <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'my-goods',
+  // 定义 props 属性，用来接收外界传递到当前组件的数据
+  props: {
+    // 商品的信息对象
+    goods: {
+      type: Object,
+      defaul: {},
+    },
+  },
+  data() {
+    return {
+      // 默认的空图片
+      defaultPic:
+        'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+    }
+  },
+  methods: {
+    gotoDetail() {
+      uni.navigateTo({
+        url: '/subpkg/goods_detail/goods_detail?goods_id=' + this.goods_id,
+      })
+    },
+  },
+  filters: {
+    // 把数字处理为带两位小数点的数字
+    tofixed(num) {
+      return Number(num).toFixed(2)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.goods-item {
+  display: flex;
+  padding: 10px 5px;
+  border-bottom: 1px solid #ccc;
+
+  .goods-item-left {
+    margin-right: 5px;
+
+    .goods-pic {
+      width: 100px;
+      height: 100px;
+      display: block;
+    }
+  }
+
+  .goods-item-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    .goods-name {
+      font-size: 13px;
+      color: #333;
+    }
+
+    .goods-info-box {
+      .goods-price {
+        font-size: 16px;
+        color: #c00000;
+      }
+    }
+  }
+}
+</style>
