008affd8fa7da99fc6031b66bf23819bec95a9c1
zhangjiaming3333158 <1093561079@qq.com>
2023-08-05 15:18:15 +0800

    switchTab

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -12,18 +12,27 @@
     </view>
 
     <!-- 结算按钮 -->
-    <view class="btn-settle">结算({{checkedCount}})</view>
+    <view class="btn-settle" @click="settlement">结算({{checkedCount}})</view>
   </view>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex'
+import { mapGetters, mapMutations, mapState } from 'vuex'
 export default {
   data() {
-    return {}
+    return {
+      // 倒计时的秒数
+      seconds: 3,
+      // 定时器的 Id
+      timer: null,
+    }
   },
   computed: {
     ...mapGetters('m_cart', ['checkedCount', 'total', 'checkedGoodsAmount']),
+    // addstr 是详细的收货地址
+    ...mapGetters('m_user', ['addstr']),
+    // token 是用户登录成功之后的 token 字符串
+    ...mapState('m_user', ['token']),
     //是否全选
     isFullCheck() {
       return this.total === this.checkedCount
@@ -32,10 +41,66 @@ export default {
   methods: {
     // 2. 使用 mapMutations 辅助函数，把 m_cart 模块提供的 updateAllGoodsState 方法映射到当前组件中使用
     ...mapMutations('m_cart', ['updateAllGoodsState']),
+    // 把 m_user 模块中的 updateRedirectInfo 方法映射到当前页面中使用
+    ...mapMutations('m_user', ['updateRedirectInfo']),
     changeAllState() {
       // 修改购物车中所有商品的选中状态
       // !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
       this.updateAllGoodsState(!this.isFullCheck)
+    },
+    settlement() {
+      // 1. 先判断是否勾选了要结算的商品
+      if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+
+      // 2. 再判断用户是否选择了收货地址
+      if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+
+      // 3. 最后判断用户是否登录了
+      if (!this.token) return this.delayNavigate()
+    },
+    // 展示倒计时的提示消息
+    showTips(n) {
+      // 调用 uni.showToast() 方法，展示提示消息
+      uni.showToast({
+        // 不展示任何图标
+        icon: 'none',
+        // 提示的消息
+        title: '请登录后再结算！' + n + ' 秒后自动跳转到登录页',
+        // 为页面添加透明遮罩，防止点击穿透
+        mask: true,
+        // 1.5 秒后自动消失
+        duration: 1500,
+      })
+    },
+    // 延迟导航到 my 页面
+    delayNavigate() {
+      // 把 data 中的秒数重置成 3 秒
+      this.seconds = 3
+      this.showTips(this.seconds)
+
+      this.timer = setInterval(() => {
+        this.seconds--
+
+        if (this.seconds <= 0) {
+          clearInterval(this.timer)
+          uni.switchTab({
+            url: '/pages/my/my',
+            // 页面跳转成功之后的回调函数
+            success: () => {
+              // 调用 vuex 的 updateRedirectInfo 方法，把跳转信息存储到 Store 中
+              this.updateRedirectInfo({
+                // 跳转的方式
+                openType: 'switchTab',
+                // 从哪个页面跳转过去的
+                from: '/pages/cart/cart',
+              })
+            },
+          })
+          return
+        }
+
+        this.showTips(this.seconds)
+      }, 1000)
     },
   },
 }
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,18 +1,30 @@
 <template>
   <view>
-
+    <my-login v-if="!token"></my-login>
+    <my-userinfo v-else></my-userinfo>
+    <!-- <my-userinfo></my-userinfo> -->
   </view>
 </template>
 
 <script>
 import badgeMix from '@/mixins/tabbar-badge.js'
+// 1. 从 vuex 中按需导入 mapState 辅助函数
+import { mapState } from 'vuex'
 export default {
   mixins: [badgeMix],
   data() {
     return {}
   },
+  computed: {
+    // 2. 从 m_user 模块中导入需要的 token 字符串
+    ...mapState('m_user', ['token']),
+  },
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+page,
+.my-container {
+  height: 100%;
+}
 </style>
